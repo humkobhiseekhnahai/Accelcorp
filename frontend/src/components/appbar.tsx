b7881166fec39245a57269a93f0fb1eebdaaafae
humkobhiseekhnahai <113964544+humkobhiseekhnahai@users.noreply.github.com>
2024-08-25 14:39:29 +0530

    updated NavBar

--- frontend/src/components/appbar.tsx
+++ frontend/src/components/appbar.tsx
@@ -20,7 +20,7 @@ export const AppBar = () => {
                 <a href="krishiAI" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">कृषि AI</a>
               </li>
               <li>
-                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                <a href="#" className="block py-2 px-3 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">वैद्य़ Care</a>
               </li>
             </ul>
           </div>
